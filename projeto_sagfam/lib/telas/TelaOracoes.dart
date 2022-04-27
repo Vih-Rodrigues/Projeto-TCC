31abaeb2e5a58f8a5ff9b8cbeac83575f6df7d1c
Vitoria Rodrigues <andrader.vitoria@gmail.com>
2022-04-26 22:04:40 -0300

    Adicionadas imagens a tela Oracoes

--- projeto_sagfam/lib/telas/TelaOracoes.dart
+++ projeto_sagfam/lib/telas/TelaOracoes.dart
@@ -31,28 +31,240 @@ class _TelaOracoesState extends State<TelaOracoes> {
       body: Container(
         margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
         child: SingleChildScrollView(
+            
             child: Column(
+              
               children: [
                 Text(
                   'Orações para antes de dormir',
                   style: TextStyle(color: Colors.white),
                 ),
-                
+
+                // 
+                // Espaçamento
+                //
+                const SizedBox(
+                        height: 10,
+                ),
+
+                //
+                // LINHA
+                //
                 SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
                   child: Row(
                     children: [
+                      
+                      //
+                      // IMAGEM 01
+                      //
                       SizedBox(
-                        width: 100,
-                        height: 100,
+                        width: 150,
+                        height: 300,
                         
                         child: Stack(
-                          
+                          children: [
+                            ShaderMask( 
+                              shaderCallback: (rect) {
+                                return const LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [Colors.white, Color.fromARGB(255, 85, 85, 85)],
+                                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                              },
+                                
+                              child: Image.asset(
+                                "lib/images/imagem01.png",
+                                width: MediaQuery.of(context).size.width,
+                              )
+                            ),
+                          ],
                         ),
-                      )
-                    ]
+                      ),
+                      
+                      const SizedBox(
+                        width: 10, // WIDTH = LARGURA, informa que o espaçamento se dará por linha
+                      ),
+
+                      //
+                      // IMAGEM02
+                      //
+                      SizedBox(
+                        width: 150,
+                        height: 300,
+                        
+                        child: Stack(
+                          children: [
+                            ShaderMask( 
+                              shaderCallback: (rect) {
+                                return const LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [Colors.white, Color.fromARGB(255, 85, 85, 85)],
+                                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                              },
+                                
+                              child: Image.asset(
+                                "lib/images/imagem02.png",
+                                width: MediaQuery.of(context).size.width,
+                              )
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      const SizedBox(
+                        width: 10,
+                      ),
+
+                      //
+                      // IMAGEM03
+                      //
+                      SizedBox(
+                        width: 150,
+                        height: 300,
+                        
+                        child: Stack(
+                          children: [
+                            ShaderMask( 
+                              shaderCallback: (rect) {
+                                return const LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [Colors.white, Color.fromARGB(255, 85, 85, 85)],
+                                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                              },
+                                
+                              child: Image.asset(
+                                "lib/images/imagem03.png",
+                                width: MediaQuery.of(context).size.width,
+                              )
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                )
+                ),
+              
+                const SizedBox(
+                  width: 5,
+                ),
+                
+                //
+                // PRÓXIMA LINHA
+                //
+
+                Text(
+                  'Orações de Nossa Senhora',
+                  style: TextStyle(color: Colors.white),
+                ),
+                
+                // 
+                // Espaçamento
+                //
+                const SizedBox(
+                  height: 10,
+                ),
+
+                //
+                // Linha com imagens 
+                //
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      //
+                      // IMAGEM04
+                      //
+                      SizedBox(
+                        width: 150,
+                        height: 300,
+                        
+                        child: Stack(
+                          children: [
+                            ShaderMask( 
+                              shaderCallback: (rect) {
+                                return const LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [Colors.white, Color.fromARGB(255, 85, 85, 85)],
+                                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                              },
+                                
+                              child: Image.asset(
+                                "lib/images/imagem04.png",
+                                width: MediaQuery.of(context).size.width,
+                              )
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      const SizedBox(
+                        width: 10,
+                      ),
+
+                      //
+                      // IMAGEM05
+                      //
+                      SizedBox(
+                        width: 150,
+                        height: 300,
+                        
+                        child: Stack(
+                          children: [
+                            ShaderMask( 
+                              shaderCallback: (rect) {
+                                return const LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [Colors.white, Color.fromARGB(255, 85, 85, 85)],
+                                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                              },
+                                
+                              child: Image.asset(
+                                "lib/images/imagem05.png",
+                                width: MediaQuery.of(context).size.width,
+                              )
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      const SizedBox(
+                        width: 10,
+                      ),
+
+                      //
+                      // IMAGEM06
+                      //
+                      SizedBox(
+                        width: 150,
+                        height: 300,
+                        
+                        child: Stack(
+                          children: [
+                            ShaderMask( 
+                              shaderCallback: (rect) {
+                                return const LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [Colors.white, Color.fromARGB(255, 85, 85, 85)],
+                                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                              },
+                                
+                              child: Image.asset(
+                                "lib/images/imagem06.png",
+                                width: MediaQuery.of(context).size.width,
+                              )
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
         ),
